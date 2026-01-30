8cc5bb9b80116dff3fbb4863e57b720ae4c60ccf
Oscar Garrido <oscar.garrido.o@ug.uchile.cl>
2026-01-30 20:14:46 -0300

    day3 part2

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,5 +1,3 @@
-use std::{cmp::max, intrinsics::powif16, io::Error};
-
 advent_of_code::solution!(3);
 
 pub fn part_one(input: &str) -> Option<u32> {
@@ -33,12 +31,12 @@ pub fn part_one(input: &str) -> Option<u32> {
   Some(joltage)
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-  let mut joltage: u32 = 0;
+pub fn part_two(input: &str) -> Option<u64> {
+  let mut joltage: u64 = 0;
 
   for bank in input.lines() {
     let base: u32 = 10;
-    let bank_digits = bank.chars().map(|c| {c.to_digit(10).expect("Failed to parse number")}).collect::<Vec<u32>>();
+    let bank_digits = bank.chars().map(|c| {c.to_digit(base).expect("Failed to parse number")}).collect::<Vec<u32>>();
     let bank_len = bank_digits.len();
     let mut batteries: Vec<u32> = vec![0; 12];
     let mut batteries_idx: Vec<usize> = vec![0; 12];
@@ -54,12 +52,13 @@ pub fn part_two(input: &str) -> Option<u32> {
 
     for batt_idx in 1..12 {
       let start_idx = batteries_idx[batt_idx - 1] + 1;
-      let end_idx = bank_len - 12 + batt_idx;
+      let end_idx = bank_len - 11 + batt_idx;
 
-      if end_idx - start_idx < 12 - batt_idx {
-
-        for i in 0..12-batt_idx {
-          batteries[batt_idx] = bank_digits[i];
+      if bank_len - start_idx < 12 - batt_idx {
+        println!("{bank_len} - {start_idx} < 12 - {batt_idx}");
+        for i in 0..13-batt_idx {
+          batteries[batt_idx+i] = bank_digits[start_idx+i];
+          batteries_idx[batt_idx+i] = start_idx+i;
         }
         break;
       }
@@ -72,11 +71,12 @@ pub fn part_two(input: &str) -> Option<u32> {
       }
     }
 
+    println!("{:?}", batteries);
+    println!("{:?}", batteries_idx);
+    println!();
 
-    let mut bank_joltage = 0;
-    for (i, batt) in batteries.iter().rev().enumerate() {
-      bank_joltage += base.pow(i as u32) * *batt as u32 ;
-    }
+    let bank_joltage: u64 = batteries.iter().fold(0, |acc, elem| acc * 10 + (*elem as u64));
+
     joltage += bank_joltage;
 
   }
@@ -96,6 +96,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(3121910778619));
     }
 }
